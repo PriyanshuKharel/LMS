858bb3ac3e3381aea356ac27286d83e812a54571
PriyanshuKharel <kharelpriyanshu01@gmail.com>
2024-01-04 21:30:35 +0545

    Feat: functionality of a website

---
+++ client/app/Page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { FC, useState } from "react";
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
+
+type Props = {};
+
+const Page = (props: Props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login");
+  return (
+    <div>
+      <Heading
+        title="ELearning"
+        description="ELearning is the platform for students to learn and get help from teachers"
+        keywords="Programming, MERN, Redux, Machine Learning, Cuber Security, Data Science, Artificial Intelligence"
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={activeItem}
+        setRoute={setRoute}
+        route={route}
+      />
+      <Hero />
+    </div>
+  );
+};
+
+export default Page;
---
+++ client/app/profile/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { useState } from "react";
+import Protected from "../hooks/useProtected";
+import Heading from "../utils/Heading";
+import Header from "../components/Header";
+import Profile from "../components/Profile/Profile";
+import { useSelector } from "react-redux";
+
+type Props = {};
+
+const page: React.FC<Props> = (props: Props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login");
+  const { user } = useSelector((state: any) => state.auth);
+  return (
+    <div>
+      <Protected>
+        <Heading
+          title={`${user?.name} profile`}
+          description="ELearning is the platform for students to learn and get help from teachers"
+          keywords="Programming, MERN, Redux, Machine Learning, Cuber Security, Data Science, Artificial Intelligence"
+        />
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          setRoute={setRoute}
+          route={route}
+        />
+        <Profile user={user} />
+      </Protected>
+    </div>
+  );
+};
+
+export default page;
